c2a86b5b10751a0b75559b656d23c758953f3d6e
Sandy <SVern99@aol.com>
2016-10-06 18:31:07 +0000

    attempting to fix precomiling assest failed to load

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module Workspace
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    
+    # Added per a stackoverflow answer to promlem pushing to Heroku that gave me the error of 'Precompiling assets faile to load'.
+    config.assets.initialize_on_precompile = false
   end
 end

da1406bb79b7e8e1f6ed8c6d4f99ef7bd5fca48b
Sandy <SVern99@aol.com>
2016-10-07 18:56:42 +0000

    changed email address in contact mailer form

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,11 +1,11 @@
 class ContactMailer < ActionMailer::Base
- default to: "info@chesnowitz.com"
+ default to: "SVern99@aol.com"
 
  def contact_email(name, email, message)
  @name = name
  @email = email
  @message = message
 
- mail(from: email, subject: 'Chesnowtiz.com Contact Form Message')
+ mail(from: email, subject: 'Sandy's Articles: Contact Form Message')
  end
 end

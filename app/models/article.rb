fc217f2d78281ce67fce9ad357685b4c6042c24e
Sandy <SVern99@aol.com>
2016-10-10 20:46:24 +0000

    added validations for uniqueness_of username, and validations for article's body and title

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,9 @@ class Article < ActiveRecord::Base
     
     belongs_to :user
     has_many :comments
+    
+    validates :title, :body, presence: true
+    validates :title, uniqueness: { message: "This article title has already been used. Please create an original article title."}
+    validates :body, length: { minimum: 15 }
+  
 end
